7193e742600a905eadb74adc79656237ab0e06de
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-03-26 23:32:20 +0100

    imports a variable

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """importing a variable a from the file variable_load_5.py"""
+    import variable_load_5 as file_val
+
+    print("{}".format(file_val.a))
