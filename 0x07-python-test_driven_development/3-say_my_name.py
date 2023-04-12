5f62b8b215cd97a7d83db5021317269c0617f806
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-04-12 22:47:39 +0100

    Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Defines a name printing function."""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints out the input name.
+
+    Args:
+        first_name(string): must be given.
+        last_name(string): optional.
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
