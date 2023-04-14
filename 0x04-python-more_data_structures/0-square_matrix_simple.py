98822ac86f2819d30eb17271cd59780dc0c2cb61
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-04-14 13:45:25 +0100

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -7,5 +7,5 @@ def square_matrix_simple(matrix=[]):
     """Returns a new matrix with square values."""
 
     if isinstance(matrix, list) and isinstance(matrix[0], list):
-        #return [[col ** 2 for col in row] for row in matrix]
-        return [list(map(lambda x : x ** 2, [col for col in row])) for row in matrix]
+        """Another way [[col ** 2 for col in row] for row in matrix] ."""
+        return [list(map(lambda x: x ** 2, [c for c in r])) for r in matrix]
