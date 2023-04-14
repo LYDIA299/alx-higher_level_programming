995ba2230f4cf7e955123a20d940923cfcd87c3e
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-04-14 15:39:09 +0100

    Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    """Converts a Roman numeral to an integer."""
+
+    if not isinstance(roman_string, str):
+        return 0
+    roman_string.upper()
+    rom_ref = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    ret = 0
+
+    for i in range(len(roman_string)):
+        ret += rom_ref[roman_string[i]]
+
+    return ret
