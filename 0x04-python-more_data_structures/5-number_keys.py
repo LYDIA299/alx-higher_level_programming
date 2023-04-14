f2a2bb99824f26ef66247aff4cae63258c4bcaf2
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-04-14 14:39:55 +0100

    Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+"""Defines number_keys function."""
+
+
+def number_keys(a_dictionary):
+    """Returns the number of keys in a dictionary."""
+
+    return (len(list(a_dictionary)))
