badb590f23a08918be9d84980374656e809fa492
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-04-15 21:56:16 +0100

    Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Defines a Rectangle class."""
+
+
+class Rectangle(BaseGeometry):
+    """Inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Constructor."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Overrides the area() in the baseclass."""
+        if self.__width and self.__height:
+            return self.__width * self.__height
+
+    def __str__(self):
+        """String magic function."""
+        if self.__width and self.__height:
+            return "[{}] {}/{}".format(str(self.__class__.__name__),
+                                           self.__width, self.__height)
