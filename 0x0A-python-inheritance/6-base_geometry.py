ca92290199b93f546625741b8edfa066c3144321
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-04-15 20:04:45 +0100

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines an empty class."""
+
+
+class BaseGeometry():
+    """Empty class."""
+
+    def area(self):
+        """Instance method."""
+        raise Exception("area() is not implemented")
