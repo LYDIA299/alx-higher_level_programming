c1ff01f845eb9ddb383f234c850213759d32d8bd
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-04-17 23:41:24 +0100

    Save Object to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#/usr/bin/python3
+
+"""Defines save_to_json_file function."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a file using JSON.
+    @ my_obj: JSON string.
+    @ filename: file to write to.
+    """
+
+    with open(filename, 'a', encoding='utf-8') as f:
+        f.write(my_obj)
