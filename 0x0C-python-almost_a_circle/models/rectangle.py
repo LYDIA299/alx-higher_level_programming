cad3802bdcf32506c839bba59c8517ec1d207ece
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-04-21 16:35:29 +0100

    First Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""Defines a Rectangle class."""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Inherits from Base class."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Class contructor.
+        @ width: width of the rectangle
+        @ height: height of the rectangle.
+        """
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, val):
+        self.__width = val
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self, val):
+        self.__height = val
+    @property
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self, val):
+        self.__x = val
+    @property
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self, val):
+        self.__y = val
