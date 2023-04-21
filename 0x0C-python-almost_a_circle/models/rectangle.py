fa73e6ebeecae33cbb62533fc13ae2b53ae362b1
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-04-21 16:39:32 +0100

    First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,24 +22,31 @@ class Rectangle(Base):
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, val):
         self.__width = val
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, val):
         self.__height = val
+
     @property
     def x(self):
         return self.__x
+
     @x.setter
     def x(self, val):
         self.__x = val
+
     @property
     def y(self):
         return self.__y
+
     @y.setter
     def y(self, val):
         self.__y = val
