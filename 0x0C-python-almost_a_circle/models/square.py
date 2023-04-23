5ccda081eb0d129c0ef2296085938b7dbe3f43c3
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-04-23 16:58:52 +0100

    Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,19 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor."""
         super().__init__(width=size, height=size, x=x, y=y, id=id)
+
     def __str__(self):
         """Overloading str in base class."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self.width
+    @size.setter
+    def size(self, val):
+        if type(val) != int:
+            raise TypeError("width must be an integer")
+        if val <= 0:
+            raise ValueError("width must be > 0")
+        self.width = val
+        self.height = val
