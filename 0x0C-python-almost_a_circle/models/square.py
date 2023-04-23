1072feb26fc6ed0c278763e49d80dd34de71bec6
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-04-23 17:14:52 +0100

    Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,3 +27,31 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = val
         self.height = val
+
+    def update(self, *args, **kwargs):
+        """Overrides the update method in the base class."""
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                """Looping through the arguments."""
+                if a == 0:
+                    self.id = arg
+                elif a == 1:
+                    self.width = arg
+                    self.height = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == 'id':
+                    self.id = v
+                elif k == 'size':
+                    self.width = v
+                    self.height = v
+                elif k == 'x':
+                    self.x = v
+                elif k == 'y':
+                    self.y = v
