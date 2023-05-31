de1c36817b4f5d02895c9de274b6892e3784211e
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-05-31 14:48:05 +0100

    Count 89

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- inserts new row(data) in the table
-INSERT INTO `first_table` (`id`, `name`) VALUES (98, "Holberto School");
+-- Inserts new row(data) in the table
+INSERT INTO first_table(`id`, `name`) VALUES(89, "Best School");
