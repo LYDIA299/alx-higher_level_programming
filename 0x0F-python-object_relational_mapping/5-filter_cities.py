f7d6d3176c2b33e52a4e4c0823263c42aa49ffe8
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-06-22 15:47:37 +0100

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""takes in name of state as an argument and lists all cities of that state"""
+"""takes in name of a state as an argument and lists all cities of that state"""
 
 if __name__ == '__main__':
 
@@ -10,21 +10,19 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name = sys.argv[4]
-    state_name = state_name.split("'")[0]
+    state_name = sys.argv[4].split("'")[0]
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
     query = "SELECT c.name FROM cities c INNER JOIN states s \
-ON c.state_id = s.id WHERE s.name = '{}' ORDER BY c.id ASC"
-    query.format(state_name)
+ON c.state_id = s.id WHERE s.name = '{}' ORDER BY c.id ASC".format(state_name)
     cur.execute(query)
     results = cur.fetchall()
     i = 1
     for row in results:
         if i < len(results):
-            print(row[0], end=", ")
+            print(row[0], end=', ')
         else:
             print(row[0])
         i = i + 1
