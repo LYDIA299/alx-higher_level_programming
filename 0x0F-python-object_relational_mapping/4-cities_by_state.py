1af14d3ec08b53111b1acb8a3355b0cbe9ace5fb
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-06-22 12:11:51 +0100

    Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""lists all cities from the database"""
+
+if __name__ == '__main__':
+
+    import sys
+    import MySQLdb
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT c.id, c.name, s.name FROM cities c INNER JOIN states s ON c.state_id = s.id ORDER BY id ASC")
+    results = cur.fetchall()
+    for row in results:
+        print(row)
+    cur.close()
+    conn.close()
