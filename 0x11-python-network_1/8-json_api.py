15bc21c671727aaa302b95bc3f7405146b069823
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-07-05 17:02:49 +0100

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,16 +8,15 @@ if __name__ == '__main__':
     import sys
     import requests
 
-    url = 'http://0.0.0.0:5000/search_user'
-    val = {'q': ""}
-    if len(sys.argv) != 1:
-        val['q'] = sys.argv[1]
-    r = requests.post(url, data=val)
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
         response = r.json()
         if response == {}:
-            print('No result')
+            print("No result")
         else:
-            print('[{}] {}'.format(response.get('id'), reponse.get('name')))
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
-        print('Not a valid JSON')
+        print("Not a valid JSON")
