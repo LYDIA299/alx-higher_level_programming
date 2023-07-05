65ebfe7361b8ddd95a3b290f502954079cf40151
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2023-07-05 12:02:46 +0100

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,7 +2,8 @@
 """ takes in a URL, sends a request URL and displays the X-Request-Id """
 
 if __name__ == '__main__':
-    import sys, urllib.request
+    import sys
+    import urllib.request
 
     req = urllib.request.Request('{}'.format(sys.argv[1]))
     with urllib.request.urlopen(req) as response:
