c33e66905b7d483173ffccb74792f482a8df272a
NAKIVUMBI LYDIA <daniellawibz@gmail.com>
2023-10-07 17:53:51 +0100

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/100-status_code.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#sends , and displays only the status code of the response
-curl -s -o /dev/null -w "%{http_code}" "$1"
--- 0x10-python-network_0/2-delete.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#sends a DELETE request to the URL and displays response body
-curl -s -X DELETE "$1"
