3330bf6be31609c7ebd1b59cf3148ae5bc0bcd11
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2024-02-17 22:16:45 +0100

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = this.height; i > 0; i--) {
+      let c = '';
+      for (let j = this.width; j > 0; j--) {
+        c = c + 'X';
+      }
+      console.log(c);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
