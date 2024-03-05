e6cc62c23d781959409add9839f096e79ee85acc
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2024-03-05 10:19:31 +0100

    resolve

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes a URL and displays HTTP methods the server accepts
-curl -sI "$url" | grep -i "Allow" | tr -d '\r' | cut -d ' ' -f2
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
