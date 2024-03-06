70da276446f0b7cb37eca73e2633f703472d19a4
LYDIA NAKIVUMBI <daniellawibz@gmail.com>
2024-03-06 17:17:32 +0100

    quick fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # fetches https://alx-intranet.hbtn.io/status
 
-import requests
-
 
 if __name__ == '__main__':
+    import requests
+
     r = requests.get('https://alx-intranet.hbtn.io/status')
-    content = r.text
+    res = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
+    print("\t- type: {}".format(type(res)))
+    print("\t- content: {}".format(res))
